4ea0435cd7af1687b66b7d3fcada19db93722141
brendagold <petersbrenda47@gmail.com>
2021-12-02 15:34:34 -0500

    changes made

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-//import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import { Route, Routes, Redirect } from "react-router-dom";
 import Footer from './components/Footer';
 import DetailsPage from './components/DetailsPage';
 import Main from './Main';
+import NotFound from './components/NotFound';
+import api from './services/api';
 
 function App() {
+  const [fullData, setFullData] = useState([])
+  const [exhibits, setExhibits] = useState([]);
+  const [togos, setTogos] = useState([]);
+  
+
+  const getExhibits = async () => {
+    const response = await api.get();
+    setFullData(response.pets)
+    console.log(fullData.pets)
+  };
+
+  useEffect(() => {
+    getExhibits();
+   
+  }, []);
   return (
     <div className="">
       <Nav />
       <Routes>
         <Route exact path="/" element={<Main />} />
         <Route path="/pet" element={<DetailsPage />} />
-        {/* <Route path="/not-found">
-          <NotFound />
-        </Route>
-        <Redirect to="/not-found" /> */}
+        <Route path="/not-found" element={<NotFound />} />
+          
+        {/* <Redirect to="/not-found" /> */}
       </Routes>
       
       <Footer />
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:"https://cdn.contentful.com//spaces/nbe8tmkxy59u/environments/master/entries?access_token=ynfiaMt1N5Skp8U-uq7Z49Xc9OSl6g-je6-cEcapCqU&content_type=exhibition"
+    baseURL:"https://pets-v2.dev-apis.com/pets"
     
     
 })
